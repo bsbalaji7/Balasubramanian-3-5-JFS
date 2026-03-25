1f7e1627a34ec01ca01545b5fcc65bda746df685
BS <bsbalaji101005@gmail.com>
2026-03-25 19:30:36 +0530

    Doneyyy

---
+++ JAVA/MyJavaAssignment/src/Pattern/NumberTriangular.java
@@ -0,0 +1,23 @@
+package Pattern;
+
+public class NumberTriangular {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int n = 4;
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+
+            System.out.println();
+		}
+	}
+
+}
