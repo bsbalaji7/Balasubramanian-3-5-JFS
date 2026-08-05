cc993ebaf8997f6f93e23a357f35dd9a7b8feab1
BS <bsbalaji101005@gmail.com>
2026-08-05 22:51:48 +0530

    Create MoveZerosToFront.java

---
+++ JAVA/MyJavaAssignment/src/Humworld/MoveZerosToFront.java
@@ -0,0 +1,27 @@
+package Humworld;
+
+public class MoveZerosToFront {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr =  {1, 0, 5, 6, 0, 1, 0, 0};
+		
+		int j = arr.length - 1;
+		for(int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] != 0) {
+				arr[j] = arr[i];
+				j--;
+			}
+		}
+		
+		while(j >= 0) {
+			arr[j] = 0;
+			j--;
+		}
+		
+		for(int num : arr) {
+			System.out.print(num + " ");
+		}
+	}
+
+}
