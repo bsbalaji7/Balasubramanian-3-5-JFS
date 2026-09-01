acb5b8d78be2ce8036a2d9f08ef9f79f913c59d1
BS <bsbalaji101005@gmail.com>
2026-09-01 18:41:17 +0530

    Update MoveZerosToFront.java

--- JAVA/MyJavaAssignment/src/Humworld/MoveZerosToFront.java
+++ JAVA/MyJavaAssignment/src/Humworld/MoveZerosToFront.java
@@ -7,19 +7,22 @@ public class MoveZerosToFront {
 		int[] arr =  {1, 0, 5, 6, 0, 1, 0, 0};
 		
 		int j = arr.length - 1;
-		for(int i = arr.length - 1; i >= 0; i--) {
-			if (arr[i] != 0) {
+		for(int i = arr.length -1; i >= 0; i--) {
+			if(arr[i] != 0) {
 				arr[j] = arr[i];
 				j--;
 			}
+			
 		}
-		
 		while(j >= 0) {
-			arr[j] = 0;
+			arr[j]=0;
 			j--;
 		}
+
 		
-		for(int num : arr) {
+		
+		
+		for(int num : arr) { 
 			System.out.print(num + " ");
 		}
 	}
